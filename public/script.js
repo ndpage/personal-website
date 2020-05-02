a0e4e112919cae8a08d1b1499150c0cbd0f2d228
ndpage <nathanpage2018@users.noreply.github.com>
2020-05-02 10:39:31 -0400

    Moved scripts in indes.html inside body tag

--- public/script.js
+++ public/script.js
@@ -1,14 +1,27 @@
 
 
-function home(){
+
     let currentdate = new Date();
     $.ajax({
       type:"GET",
-      url:"https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT",
+      url:
       success: function(data) {
           $('.text').prepend('<p>' + data.symbol + " $" + data.price +" as of " + currentdate + '</p>');
         },
       dataType: 'json',
     });
-  }
-  
+
+    $('.submit-btn').click(function() {
+      $.ajax({
+        type: "POST",
+        url: "login",
+        contentType: "application/json",
+        data: JSON.stringify({
+          username: $("#username").val(),
+          password: $("#password").val()
+        }),
+        success: function(data) {
+          console.log('data', data);
+        }
+      })
+    });
