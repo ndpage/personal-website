bbac34c89d68711e4a499b4859c68183c965f3c0
ndpage <nathanpage2018@users.noreply.github.com>
2020-05-02 09:54:59 -0400

    Added express and body parser to server.js

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 
+// Package declarations for this server.js file
 var express= require('express');
+var bodyParser = require('body-parser')
 var app = express();
 
+// Configuation for the server app
+app.use(express.static(__dirname + '/public')); // Server everything in the public directory
+app.use(bodyParser.json());
 
 
-app.use(express.static(__dirname + '/public'));
 
-app.get('/hello.txt', function(req, res){
+app.get('/home', function(req, res){
     res.send('Hello World');
   });
   
 
-
-
-  // Start the server
-  var server = app.listen(8080, function() {
-      console.log('Listening on port 8080');
-  });
+// Start the server
+var server = app.listen(8080, function() {
+    console.log('Listening on port 8080');
+});
