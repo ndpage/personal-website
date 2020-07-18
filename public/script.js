715672beec87109747c2a40c002b66e7bf2d8905
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-07-18 19:04:57 -0400

    Added event listener to button. Works as expected.

--- public/script.js
+++ public/script.js
@@ -1,18 +1,9 @@
 
 
-<<<<<<< HEAD
-    $('.header-btn-projects').click(function() {
-      alert("Button clicked");
-      $.ajax({
-        type: "GET",
-        url: "projects",
-        contentType: "text/html",
-        success: function(data) {
-          console.log("Success!");
-        }
-      })    
-=======
     $('.profile-img').click(function() { //Event listener for home button 
       alert("clicked image");
->>>>>>> b57b4c1f750b86b1908c4ecca3644e223c00a738
+    });
+
+    $('.projects-btn').click(function() { //Event listener for home button 
+      alert("clicked projects button");
     });
