110f12247411c869dbe36a653d3e3defb69a9f80
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-07-24 22:11:12 -0400

    Added code to get content-main id and set the inner HTML

--- public/script.js
+++ public/script.js
@@ -6,4 +6,10 @@
 
     $('.projects-btn').click(function() { //Event listener for home button 
       alert("Pressed the button");
+
+      var contentMain = document.getElementById("content-main")
+
+          contentMain.innerHTML = 
+            "<div> <h1>This is the projects page</h1></div>"
     });
+    
