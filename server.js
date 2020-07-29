13df0a15b5b8568e3efb6f13bce3b7996f9a24f4
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-07-28 23:23:39 -0400

    Added app.get for /projects url

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 
 // Package declarations for this server.js file
 
-var express= require('express');
-// var bodyParser = require('body-parser')syn
+var express= require('express'); 
+var bodyParser = require('body-parser');
 var app = express();
 var port = 3000;
 var hostname = '127.0.0.1';
 // Configuation for the server app
 
 app.use(express.static(__dirname + '/public')); // Serve everything in the public directory
-
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 app.get('/', (reg,res)=>res.sendFile(__dirname + '/public/index.html'));  
-app.get('/hello-world', (req, res) => res.send('Hello World!'));
-app.get('/projects', (req,res)=>res.sendFile(__dirname + '/projects.html'));
+
+//app.get('/hello-world', function(req, res) => res.send('Hello World!'));
+
+app.get('/projects', function(req,res){
+        res.sendFile(__dirname + '/projects.html');  
+    }
+);
 
 // Start the server
 app.listen(port, hostname); //127.0.0.1
