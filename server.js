c10b00a56eba0670b14991225574e9b696a01b37
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-08-04 12:04:13 -0400

    Updated location of projects.html in res.sendFile

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = 3000;
 var hostname = '127.0.0.1';
+
+app.use(bodyParser.json());
+
 // Configuation for the server app
 
 app.use(express.static(__dirname + '/public')); // Serve everything in the public directory
-app.use(bodyParser.json());
 
 app.get('/home', (reg,res)=>res.sendFile(__dirname + '/public/index.html'));  
+
+app.get('/projects', (req,res) => res.sendFile(__dirname + '/public/projects.html')); 
 
 app.get('/admin',
     function(request, response){
@@ -24,10 +28,6 @@ app.get('/admin',
 );
 //app.get('/hello-world', function(req, res) => res.send('Hello World!'));
 
-app.get('/projects', function(req,res){
-         res.sendFile(__dirname + '/projects.html');  
-    }
-);
 
 // Start the server
 app.listen(port, hostname); //127.0.0.1
